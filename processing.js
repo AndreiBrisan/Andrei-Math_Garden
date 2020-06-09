3ebaaf1301664e47cd95b1704f7820ba917077b4
AndreiBrisan <45906613+AndreiBrisan@users.noreply.github.com>
2020-06-09 20:55:14 +0300

    Update processing.js

--- processing.js
+++ processing.js
@@ -25,11 +25,13 @@ function predictImage() {
         height = 20;
         const scaleFactor = image.rows / height;
         width = Math.round(image.cols / scaleFactor);
+
     }
     else {
         width = 20;
         const scaleFactor = image.cols / width;
         height = Math.round(image.rows / scaleFactor);
+
     }
 
     let newSize = new cv.Size(width, height);
@@ -39,6 +41,8 @@ function predictImage() {
     const RIGHT = Math.floor(4 + (20 - width) / 2);
     const TOP = Math.ceil(4 + (20 - height) / 2);
     const BOTTOM = Math.floor(4 + (20 - height) / 2);
+
+    // console.log('Top:' + TOP + ' BOTTOM:' + BOTTOM + ' LEFT:' + LEFT + ' RIGHT:' + RIGHT);
 
     const BLACK = new cv.Scalar(0, 0, 0, 0);
     cv.copyMakeBorder(image, image, TOP, BOTTOM, LEFT, RIGHT, cv.BORDER_CONSTANT, BLACK);
@@ -54,12 +58,13 @@ function predictImage() {
     const X_SHIFT = Math.round(image.cols / 2.0 - cx);
     const Y_SHIFT = Math.round(image.rows / 2.0 - cy);
 
+
     newSize = new cv.Size(image.cols, image.rows);
     const M = cv.matFromArray(2, 3, cv.CV_64FC1, [1, 0, X_SHIFT, 0, 1, Y_SHIFT]);
     cv.warpAffine(image, image, M, newSize, cv.INTER_LINEAR, cv.BORDER_CONSTANT, BLACK);
 
     let pixelValues = image.data;
-    // console.log('Pixel Values: ' + pixelValues);
+
     pixelValues = Float32Array.from(pixelValues);
 
     pixelValues = pixelValues.map(function (item) {
@@ -67,11 +72,17 @@ function predictImage() {
     });
 
     const X = tf.tensor([pixelValues]);
-
     result = model.predict(X);
     result.print();
 
     const output = result.dataSync()[0];
+
+    // console.log(tf.memory());
+
+    // Testing only
+    // const outputCanvas = document.createElement('CANVAS');
+    // cv.imshow(outputCanvas, image);
+    // document.body.appendChild(outputCanvas);
 
     // cleanup
     image.delete();
